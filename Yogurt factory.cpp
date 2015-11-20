f6b0553d698bc24b71e590006cf23c5a4cc7bb0a
Wangbicong <wangbicong@hotmail.com>
2015-11-21 02:32:18 +0800

    add yogurt factory.cpp

---
+++ Yogurt factory.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int n,N;
+    int S;
+    int price,count;
+    long long int sum;
+    int past;
+    sum = 0;
+    scanf("%d %d",&N,&S);
+    for(n=0;n<N;n++){
+        scanf("%d %d",&price,&count);
+        if(n){
+            price = price < past + S ? price : past + S;
+        }
+        sum += price * count;
+        past = price;
+    }
+    printf("%lld\n",sum);
+    return 0;
+}
